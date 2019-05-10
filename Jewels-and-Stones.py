e66c17a3c65b15e8186a1ff43f7cfee532968820
Aniket Gade <34727005+ag93@users.noreply.github.com>
2019-05-09 19:36:23 -0500

    Add files via upload

---
+++ Jewels-and-Stones.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        jewels = set(J)
+        return sum(s in jewels for s in S)
