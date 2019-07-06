a3b46aef80ca59c4bb6d5ecc21e0128d72127866
Aniket Gade <34727005+ag93@users.noreply.github.com>
2019-07-05 21:21:21 -0500

    Add files via upload

---
+++ Sum-of-2-Numbers.py
@@ -0,0 +1,5 @@
+#Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        return(sum([a,b]))
