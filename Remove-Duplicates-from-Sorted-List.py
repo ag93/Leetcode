557a3ff678fef10d3ba60feb53f1d13a47551931
Aniket Gade <34727005+ag93@users.noreply.github.com>
2019-07-20 23:03:21 -0500

    Add files via upload

---
+++ Remove-Duplicates-from-Sorted-List.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head == None:
+            return head
+        
+        current = head.next
+        prev = head
+        
+        while current != None:
+            if current.val == prev.val:
+                prev.next = current.next
+                current = current.next
+            else:
+                current = current.next
+                prev = prev.next
+        
+        return head
