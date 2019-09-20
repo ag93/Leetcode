07308247308f81ed3c3890adeea412d5e4c4c820
Aniket Gade <34727005+ag93@users.noreply.github.com>
2019-09-19 21:14:34 -0500

    Add files via upload

---
+++ Preorder DFS.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        if root is None:
+            return([])
+        
+        stack = [root]
+        output = []
+        
+        while stack:
+            node = stack.pop()
+            if node is not None:
+                output.append(node.val)
+                if node.right is not None:
+                    stack.append(node.right)
+                if node.left is not None:
+                    stack.append(node.left)
+        return output
