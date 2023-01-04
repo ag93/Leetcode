6868b8682150fbe2c654a6c4cf70dc44ca1f13dd
ag93 <aniketgade03@gmail.com>
2023-01-03 18:01:39 -0600

    Leetcode #977 - Squares of a Sorted Array

--- Squares-of-Sorted-Array.py
+++ Squares-of-Sorted-Array.py
@@ -1,3 +1,30 @@
-class Solution(object):
-    def sortedSquares(self, A):
-        return sorted(x*x for x in A)
+# class Solution(object):
+#     def sortedSquares(self, A):
+#         return sorted(x*x for x in A)
+
+def sortedSquares(nums):
+    """
+    :type nums: List[int]
+    :rtype: List[int]
+    """
+    n = len(nums)
+    left = 0
+    right = n-1
+
+    result = [0]*n
+
+    for i in range(n-1, -1, -1):
+        leftSquare = nums[left]*nums[left]
+        rightSquare = nums[right]*nums[right]
+        if leftSquare > rightSquare:
+            result[i] = leftSquare
+            left+=1
+        else:
+            result[i] = rightSquare
+            right-=1
+
+    return result
+
+if __name__ == '__main__':
+    nums = [-4, -1, 0, 3, 10]
+    print(sortedSquares(nums))
